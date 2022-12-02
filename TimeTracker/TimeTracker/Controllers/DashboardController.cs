dba62961f447c3e948a5b5f41d53360af5833e71
Nitin Lakum <lakumnitin1@outlook.com>
2022-12-02 11:19:29 +0530

    Changes

--- TimeTracker/TimeTracker/Controllers/DashboardController.cs
+++ TimeTracker/TimeTracker/Controllers/DashboardController.cs
@@ -9,7 +9,7 @@ using TimeTracker_Repository;
 
 namespace TimeTracker.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class DashboardController : Controller
     {
         private readonly ISettingRepo _settingRepo;
@@ -33,70 +33,83 @@ namespace TimeTracker.Controllers
 
         private async Task ListOfResources(ResourceViewModel model)
         {
-            HttpClient _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri($"https://prod.hirect.ai/hirect/candidate-service/candidates/search?searchDirect=true&cityId=356&qs={model.Designation}&pageNum=0&pageSize=20");
-            _httpClient.DefaultRequestHeaders.Accept.Clear();
-            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            _httpClient.DefaultRequestHeaders.Add("authority", "prod.hirect.ai");
-            _httpClient.DefaultRequestHeaders.Add("accept", "application/json, text/plain, */*");
-            _httpClient.DefaultRequestHeaders.Add("accept-language", "en-US,en;q=0.9");
-            _httpClient.DefaultRequestHeaders.Add("origin", "https://recruiter.hirect.in");
-            _httpClient.DefaultRequestHeaders.Add("referer", "https://recruiter.hirect.in/");
-            _httpClient.DefaultRequestHeaders.Add("region", "in");
-            _httpClient.DefaultRequestHeaders.Add("sec-ch-ua", "\"Microsoft Edge\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"");
-            _httpClient.DefaultRequestHeaders.Add("sec-ch-ua-mobile", "?0");
-            _httpClient.DefaultRequestHeaders.Add("sec-ch-ua-platform", "\"Windows\"");
-            _httpClient.DefaultRequestHeaders.Add("sec-fetch-dest", "empty");
-            _httpClient.DefaultRequestHeaders.Add("sec-fetch-mode", "cors");
-            _httpClient.DefaultRequestHeaders.Add("sec-fetch-site", "cross-site");
-            _httpClient.DefaultRequestHeaders.Add("x-appversion", "2.1.0");
-            _httpClient.DefaultRequestHeaders.Add("x-brand", "Windows");
-            _httpClient.DefaultRequestHeaders.Add("x-deviceid", "985300e24227239fe9f5b338b930473d");
-            _httpClient.DefaultRequestHeaders.Add("x-idtoken", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZWIwMmY0OTgwYTg1NDZhMmFkOTFmMjNlNTM5OCIsImNyZWF0ZVRpbWUiOjE2Njk2MTE1NDQ3MTQsImlhdCI6MTY2OTYxMTU0NCwiaXNzIjoiaGlyZWN0X2FwcF9pc3N1ZXIifQ.TyHQntYrIIx7bsnxl42Lcgp4fSnsYrd5CoENBr0hHls");
-            _httpClient.DefaultRequestHeaders.Add("x-model", "10");
-            _httpClient.DefaultRequestHeaders.Add("x-os", "webapp");
-            _httpClient.DefaultRequestHeaders.Add("x-region", "in");
-            _httpClient.DefaultRequestHeaders.Add("x-role", "1");
-            _httpClient.DefaultRequestHeaders.Add("x-timestamp", "1669797482071");
-            _httpClient.DefaultRequestHeaders.Add("x-uid", "eb02f4980a8546a2ad91f23e5398");
-
-            var content = JsonContent.Create(new
+            for (int i = 0; i < i + 1; i++)
             {
-                education = new string[] { model.education },
-                experience = new string[] { model.experience },
-                salary = new string[] { model.salary },
-                gender = 0,
-                minAge = model.minAge,
-            });
+                string url = $"https://prod.hirect.ai/hirect/candidate-service/candidates/search?searchDirect=true&cityId={model.CityId}&qs={model.Designation}&pageNum={i}&pageSize=20";
 
-            var response = await _httpClient.PostAsync("", content);
+                HttpClient _httpClient = new HttpClient();
+                _httpClient.BaseAddress = new Uri(url);
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            if (response.IsSuccessStatusCode)
-            {
-                var result = response.Content.ReadAsStringAsync().Result;
+                _httpClient.DefaultRequestHeaders.Add("authority", "prod.hirect.ai");
+                _httpClient.DefaultRequestHeaders.Add("accept", "application/json, text/plain, */*");
+                _httpClient.DefaultRequestHeaders.Add("accept-language", "en-US,en;q=0.9");
+                _httpClient.DefaultRequestHeaders.Add("origin", "https://recruiter.hirect.in");
+                _httpClient.DefaultRequestHeaders.Add("referer", "https://recruiter.hirect.in/");
+                _httpClient.DefaultRequestHeaders.Add("region", "in");
+                _httpClient.DefaultRequestHeaders.Add("sec-ch-ua", "\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"");
+                _httpClient.DefaultRequestHeaders.Add("sec-ch-ua-mobile", "?0");
+                _httpClient.DefaultRequestHeaders.Add("sec-ch-ua-platform", "\"Windows\"");
+                _httpClient.DefaultRequestHeaders.Add("sec-fetch-dest", "empty");
+                _httpClient.DefaultRequestHeaders.Add("sec-fetch-mode", "cors");
+                _httpClient.DefaultRequestHeaders.Add("sec-fetch-site", "cross-site");
+                _httpClient.DefaultRequestHeaders.Add("x-appversion", "2.1.0");
+                _httpClient.DefaultRequestHeaders.Add("x-brand", "Windows");
+                _httpClient.DefaultRequestHeaders.Add("x-deviceid", "792de51e4d5be52a35f55f3570193fc3"); //Change
+                _httpClient.DefaultRequestHeaders.Add("x-idtoken", $"{model.Token}");
+                _httpClient.DefaultRequestHeaders.Add("x-model", "10");
+                _httpClient.DefaultRequestHeaders.Add("x-os", "webapp");
+                _httpClient.DefaultRequestHeaders.Add("x-region", "in");
+                _httpClient.DefaultRequestHeaders.Add("x-role", "1");
+                _httpClient.DefaultRequestHeaders.Add("x-timestamp", "1669877252654"); //Change
+                _httpClient.DefaultRequestHeaders.Add("x-uid", "eb02f4980a8546a2ad91f23e5398");
 
-                var deserializeObject = JsonConvert.DeserializeObject<dynamic>(result);
-                var list = deserializeObject["data"]["list"];
-
-                foreach (var item in list)
+                string[] education = model.education.Split(',');
+                string[] experience = model.experience.Split(',');
+                string[] salary = model.salary.Split(',');
+                var content = JsonContent.Create(new
                 {
-                    string id = item["id"];
-                    string preferenceId = item["preferenceId"];
+                    education = education,
+                    experience = experience,
+                    salary = salary,
+                    gender = 0,
+                    minAge = 18,
+                });
 
-                    var resource = await _settingRepo.GetResourceById(id);
-                    if (resource == null || resource.Id == 0)
+                var response = await _httpClient.PostAsync("", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = response.Content.ReadAsStringAsync().Result;
+
+                    var deserializeObject = JsonConvert.DeserializeObject<dynamic>(result);
+                    var list = deserializeObject["data"]["list"];
+
+                    if (((Newtonsoft.Json.Linq.JArray)list).Count == 0)
+                    { break; }
+
+                    foreach (var item in list)
                     {
-                        await GetResourceDetails(id, preferenceId);
+                        string id = item["id"];
+                        string preferenceId = item["preferenceId"];
+
+                        var resource = await _settingRepo.GetResourceById(id);
+                        if (resource == null || resource.Id == 0)
+                        {
+                            await GetResourceDetails(id, preferenceId, model.Token);
+                        }
                     }
                 }
             }
         }
 
-        private async Task GetResourceDetails(string id, string preferenceId)
+        private async Task GetResourceDetails(string id, string preferenceId, string token)
         {
+            string url = $"https://prod.hirect.ai/hirect/candidate-service/recruiters/candidates/{id}/profile?preferenceId={preferenceId}";
+
             HttpClient _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri($"https://prod.hirect.ai/hirect/candidate-service/recruiters/candidates/{id}/profile?preferenceId={preferenceId}");
+            _httpClient.BaseAddress = new Uri(url);
             _httpClient.DefaultRequestHeaders.Accept.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
@@ -115,13 +128,13 @@ namespace TimeTracker.Controllers
             _httpClient.DefaultRequestHeaders.Add("sec-fetch-site", "cross-site");
             _httpClient.DefaultRequestHeaders.Add("x-appversion", "2.1.0");
             _httpClient.DefaultRequestHeaders.Add("x-brand", "Windows");
-            _httpClient.DefaultRequestHeaders.Add("x-deviceid", "985300e24227239fe9f5b338b930473d");
-            _httpClient.DefaultRequestHeaders.Add("x-idtoken", "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZWIwMmY0OTgwYTg1NDZhMmFkOTFmMjNlNTM5OCIsImNyZWF0ZVRpbWUiOjE2Njk2MTE1NDQ3MTQsImlhdCI6MTY2OTYxMTU0NCwiaXNzIjoiaGlyZWN0X2FwcF9pc3N1ZXIifQ.TyHQntYrIIx7bsnxl42Lcgp4fSnsYrd5CoENBr0hHls");
+            _httpClient.DefaultRequestHeaders.Add("x-deviceid", "792de51e4d5be52a35f55f3570193fc3"); //Change
+            _httpClient.DefaultRequestHeaders.Add("x-idtoken", $"{token}");
             _httpClient.DefaultRequestHeaders.Add("x-model", "10");
             _httpClient.DefaultRequestHeaders.Add("x-os", "webapp");
             _httpClient.DefaultRequestHeaders.Add("x-region", "in");
             _httpClient.DefaultRequestHeaders.Add("x-role", "1");
-            _httpClient.DefaultRequestHeaders.Add("x-timestamp", "1669797197438");
+            _httpClient.DefaultRequestHeaders.Add("x-timestamp", "1669877252654"); //Change
             _httpClient.DefaultRequestHeaders.Add("x-uid", "eb02f4980a8546a2ad91f23e5398");
 
             var response = await _httpClient.GetAsync("");
--- TimeTracker/TimeTracker/Models/Setting/ResourceViewModel.cs
+++ TimeTracker/TimeTracker/Models/Setting/ResourceViewModel.cs
@@ -3,9 +3,10 @@
     public class ResourceViewModel
     {
         public string Designation { get; set; }
-        public string education { get; set; } = "1";
-        public string experience { get; set; } = "4";
-        public string salary { get; set; } = "1";
-        public int minAge { get; set; }
+        public string education { get; set; }
+        public string experience { get; set; }
+        public string salary { get; set; }
+        public int CityId { get; set; }
+        public string Token { get; set; }
     }
 }
