4752f9a9b9cb219f9f93f2c8c69c54b59567149b
Nitin Lakum <lakumnitin1@outlook.com>
2022-12-13 16:00:17 +0530

    changes

--- TimeTracker/TimeTracker/SqlScript/01_Pre_Config.sql
+++ TimeTracker/TimeTracker/SqlScript/01_Pre_Config.sql
@@ -4,9 +4,17 @@ INSERT INTO dbo.Roles (Id, [Name], DisplayName, Deleted) VALUES (1, 'Admin', 'Ad
 INSERT INTO dbo.Roles (Id, [Name], DisplayName, Deleted) VALUES (2, 'Employee', 'Employee', 0)
 SET IDENTITY_INSERT dbo.Roles OFF;
 
-INSERT INTO dbo.Users (RoleId,Username,FullName,Email,Designation,Education,Experience,ContactNo,Gender,Password,Address,DOB,
-JoiningDate,CreateAt,BankName,AccountNo,IFSC,MacAddress)
-VALUES (1,'lakumpankaj','LakumPankaj','lakumpankaj666@gmail.com','Developer','Bcom','1 Year','8758989031',0,'NvOnm/wkghjqO4pVGBPzCg==',surat,12-07-1997,01-01-2022,01-01-2022,'BOB',1254521254,'AHJHD154','E0D55E662AA0')
+
+/*** Added a preloaded User. ***/
+SET IDENTITY_INSERT dbo.[Users] ON;
+INSERT INTO [dbo].[Users]([Id],[RoleId],[Username],[FullName],[Email],[Designation],[Education],[Experience]
+           ,[ContactNo],[Gender],[Password],[Address],[DOB],[JoiningDate],[CreateAt],[BankName],[AccountNo]
+           ,[IFSC],[MacAddress],[Url])
+VALUES(1,1,'Admin','WhiteCore Technology','info@whitecoregroup.com','N/A','N/A','N/A','8905560307',1,'XVsc0JBTHsUQc5++Fe1auA=='
+		,'428, WhiteCore Technology LLP, Avadh viceroy, Sarthana Jakat Naka, Nana Varachha, Surat, Gujarat-395006',null
+		,GETDATE(),GETDATE(),'N/A','N/A','N/A','N/A','')
+SET IDENTITY_INSERT dbo.[Users] OFF;
+
 
 /*** Added a preloaded Settings. ***/
 IF NOT EXISTS(SELECT 1 FROM dbo.Settings WHERE [Key]='SYSTEM_WIFI_NAME')
@@ -18,13 +26,13 @@ END
 IF NOT EXISTS(SELECT 1 FROM dbo.Settings WHERE [Key]='SYSTEM_LOG_START_TIMING')
 BEGIN
 	INSERT INTO dbo.Settings ([Key], DisplayName, [Value], IsActive)
-	VALUES ('SYSTEM_LOG_START_TIMING', 'System Log Start Timing', '8:0', 1)
+	VALUES ('SYSTEM_LOG_START_TIMING', 'System Log Start Timing', '08:00', 1)
 END
 
 IF NOT EXISTS(SELECT 1 FROM dbo.Settings WHERE [Key]='SYSTEM_LOG_END_TIMING')
 BEGIN
 	INSERT INTO dbo.Settings ([Key], DisplayName, [Value], IsActive)
-	VALUES ('SYSTEM_LOG_END_TIMING', 'System Log End Timing', '20:0', 1)
+	VALUES ('SYSTEM_LOG_END_TIMING', 'System Log End Timing', '20:00', 1)
 END
 
 IF NOT EXISTS(SELECT 1 FROM dbo.Settings WHERE [Key]='COMPANY_HOLIDAYS')
